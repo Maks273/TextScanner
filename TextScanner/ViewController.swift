01c5b1df77bd0a0bb2ed09947296690ecc73d850
maks <root@localhosst>
2021-05-05 16:09:24 +0300

    Initial Commit

---
+++ TextScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextScanner
+//
+//  Created by Maksym Paidych on 05.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
